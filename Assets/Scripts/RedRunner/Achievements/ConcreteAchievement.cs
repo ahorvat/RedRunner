541ef1971d535caf0ab3a2c1cd2ad57967ffa91f
Martijn van der Meer <martijn.vander.meer@quicknet.nl>
2019-06-10 12:33:34 +0200

    Initial Commit

---
+++ Assets/Scripts/RedRunner/Achievements/ConcreteAchievement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteAchievement : AchievementBase
+{
+    private string _state;
+
+    public string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+}
---
+++ Assets/Scripts/RedRunner/Achievements/ConcreteObserver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcreteObserver : Observer
+{
+    private ConcreteAchievement _achievement;
+
+    public ConcreteObserver(ConcreteAchievement achievement)
+    {
+        _achievement = achievement;
+    }
+
+    public override void Update()
+    {
+        Debug.Log("100 Coins!");
+    }
+}
